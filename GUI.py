74dd9671be65e0e0a446a6768df7a18af3b2488b
Tahmid Zaman <tahmidzaman@gmail.com>
2018-02-25 20:10:09 +0000

    0.1.2: Imported additional Kivy functions and added widgets to GUI build. Started integrating Inventory data into the GUI.

--- GUI.py
+++ GUI.py
@@ -1,13 +1,24 @@
-import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
-
+# Kivy imports
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+# Vending machine imports
+import Inventory
+
+# BaseGUI definition
+class BaseGUI(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(BaseGUI, self).__init__(**kwargs) # this has to be here, trust me...
+        self.cols = 2 # sets two columns. Added widgets are inserted to the columns going left to right.
+        self.add_widget(Label(text=Inventory.itemNames[0]))
+        self.add_widget(Label(text=str(Inventory.itemPrices[0])))
+
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return BaseGUI() # Build according to the BaseGUI.
 
 def DisplayGUI():
     if __name__ == '__main__':
