9fb881e3d57402f7d8bbae3280b5f20726fc6387
Tahmid Zaman <tahmidzaman@gmail.com>
2018-03-04 18:19:04 +0000

    0.3.0: Expanded inventory size and added images, made for loop dynamic according to inventory size.

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 # Vending machine imports
 import TransactionHandler
 import Inventory
@@ -23,10 +24,11 @@ class BaseGUI(GridLayout):
         global walletLabel
         global brokeLabel
         super(BaseGUI, self).__init__(**kwargs) # this has to be here, trust me...
-        self.cols = 3 # sets three columns. Added widgets are inserted to the columns going left to right.
+        self.cols = 4 # sets three columns. Added widgets are inserted to the columns going left to right.
 
-        for x in range(0, 3): # creates widgets for item names, item prices, and buttons through iteration.
+        for x in range(0, len(Inventory.itemNames)): # creates widgets for item names, item prices, and buttons through iteration.
             self.add_widget(Label(text= Inventory.itemNames[x]))
+            self.add_widget(Image(source = Inventory.itemImages[x]))
             self.add_widget(Label(text= Inventory.as_currency(Inventory.itemPrices[x])))
             btn1 = Button(text="buy") # result of Button function assigned to btn1.
             btn1.bind(on_press = partial(TransactionHandler.IsTransactionPossible, x)) # IsTransactionPossible is being delegated with a given parameter to the on_press event of btn1.
--- Inventory.py
+++ Inventory.py
@@ -1,5 +1,6 @@
-itemNames = ["Coke", "Pepsi", "Fanta"]
-itemPrices = [0.99, 8.0, 5.0]
+itemNames = ["Coke", "Pepsi", "Fanta", "Dr Pepper", "Redbull", "Monster"]
+itemImages = ["coke.jpg", "pepsi.jpg", "fanta.jpg", "dr-pepper.jpg", "redbull.jpeg", "monster.jpg"]
+itemPrices = [0.99, 8.0, 5.0, 0.50, 0.49, 0.01]
 
 def as_currency(amount):
     if amount >= 0:
