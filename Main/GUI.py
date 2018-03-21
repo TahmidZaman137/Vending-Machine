290ac810ad25bdf994dfb45693b295bf3dfd5f40
Tahmid Zaman <tahmidzaman@gmail.com>
2018-03-21 15:27:28 +0000

    0.6.0: Added item descriptions to each item of the vending machine. Removed black bar from the top of the GUI. Edited widgets' text wrapping using the Kivy language.

--- Main/GUI.py
+++ Main/GUI.py
@@ -14,7 +14,6 @@ from kivy.app import runTouchApp
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-from kivy.core.audio import SoundLoader
 
 # Vending machine imports
 import TransactionHandler
@@ -28,7 +27,7 @@ class BaseGUI(GridLayout):
 
     def __init__(self, **kwargs): # defines initial properties of the GUI.
         super(BaseGUI, self).__init__(**kwargs) # this has to be here, trust me...
-        self.cols = 4
+        self.cols = 5
         self.size_hint_x = 1
         self.size_hint_y = 2
         self.spacing = 20
@@ -37,9 +36,10 @@ class BaseGUI(GridLayout):
             self.add_widget(Label(text=Inventory.drinks[x].name))
             self.add_widget(Image(source=Inventory.drinks[x].image))
             self.add_widget(Label(text=Inventory.as_currency(Inventory.drinks[x].price)))
-            btn1 = Button(text="buy") # result of Button function assigned to btn1.
+            btn1 = Button(text="Buy") # result of Button function assigned to btn1.
             btn1.bind(on_press = partial(TransactionHandler.IsTransactionPossible, x)) # IsTransactionPossible is being delegated with a given parameter to the on_press event of btn1.
             self.add_widget(btn1)
+            self.add_widget(Label(text=Inventory.drinks[x].description))
 
         walletLabel = Label(text=Inventory.as_currency(TransactionHandler.userWallet))
         TransactionHandler.walletLabelReference = walletLabel
@@ -52,7 +52,7 @@ class BaseGUI(GridLayout):
 
 
         # creates vertical scroll bar widget
-        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        root = ScrollView(size_hint=(1, 1), size=(Window.width, Window.height))
         root.add_widget(self)
         runTouchApp(root)
 
--- Main/Inventory.py
+++ Main/Inventory.py
@@ -5,18 +5,19 @@
 #===================================================================================
 
 class Item:
-    def __init__(self, name, price):
+    def __init__(self, Name, Price, Description):
         pathToImages = r"C:\Users\tahmi\OneDrive\Documents\Git\Vending-Machine\Images\\"
-        self.name = name
+        self.name = Name
         self.image = pathToImages + self.name + ".jpg"
-        self.price = price
+        self.price = Price
+        self.description = Description
 
-coke = Item("Coke", 0.99)
-pepsi = Item("Pepsi", 8.0)
-fanta = Item("Fanta", 5.0)
-drpepper = Item("Dr Pepper", 0.50)
-redbull = Item("Redbull", 0.49)
-monster = Item("Monster", 0.01)
+coke = Item("Coke", 0.99, "Coca-Cola is the most popular soft drink in history, as well as the best-known brand in the world.")
+pepsi = Item("Pepsi", 8.0, "Pay each penny to save Israel.")
+fanta = Item("Fanta", 5.0,"Bright, bubbly and instantly refreshing, Fanta is made with 100% natural flavors and is caffeine free.")
+drpepper = Item("Dr Pepper", 0.50, "The unique taste of 'Dr Pepper' - 'What's the worst that could happen?")
+redbull = Item("Redbull", 0.49, "Red bull energy drink is a functional beverage providing wings whenever you need them.")
+monster = Item("Monster", 0.01, "Tear into a can of Monster Energy,\n the meanest energy drink on the planet.")
 
 drinks = [coke, pepsi, fanta, drpepper, redbull, monster]
 
